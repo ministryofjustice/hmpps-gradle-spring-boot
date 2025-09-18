96017daf4a5fb8826311eef6b2784425f7e893fd
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-09-18 15:41:59 +0100

    SDIT-3053: ⬆️  Upgrade to spring boot 3.5.6 & kotlin 2.2.20 (#381)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
@@ -14,7 +14,6 @@ class DependencyManagementPluginManager(override val project: Project) : PluginM
   override fun configure() {
     applyDependencyManagementBom(project)
     project.extensions.extraProperties["opentelemetry.version"] = OPENTELEMETRY_VERSION
-    project.extensions.extraProperties["netty.version"] = "4.1.125.Final"
   }
 
   private fun applyDependencyManagementBom(project: Project) {
