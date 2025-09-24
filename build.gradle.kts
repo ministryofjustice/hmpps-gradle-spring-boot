33475e6c5b78e61092b2f83df90204b3a50fb60c
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-09-24 10:57:25 +0100

    SDIT-3057: 🔒️ Suppress false positive for trusted boot (#383)

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "9.1.0"
+version = "9.1.1"
 
 gradlePlugin {
   website.set("https://github.com/ministryofjustice/hmpps-gradle-spring-boot")
