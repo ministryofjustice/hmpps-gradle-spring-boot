db7d392c3239838723e9d5ac839b57efc762ef33
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-10-06 08:04:22 +0100

    CPR-926 pin logback to avoid CVE-2025-11226

--- src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
@@ -14,6 +14,7 @@ class DependencyManagementPluginManager(override val project: Project) : PluginM
   override fun configure() {
     applyDependencyManagementBom(project)
     project.extensions.extraProperties["opentelemetry.version"] = OPENTELEMETRY_VERSION
+    project.extensions.extraProperties["logback.version"] = "1.5.19"
   }
 
   private fun applyDependencyManagementBom(project: Project) {
