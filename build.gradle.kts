150d55ecd4f3ed2540b0595161fc42522ba08c40
samz-justice <sam.zaroug@justice.gov.uk>
2026-01-23 11:05:36 +0000

    CPR-1033 update version to 10.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "10.0.0"
+version = "10.0.1"
 
 gradlePlugin {
   website.set("https://github.com/ministryofjustice/hmpps-gradle-spring-boot")
