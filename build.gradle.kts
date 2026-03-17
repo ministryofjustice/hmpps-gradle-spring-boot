06401fd312552469f259397852d93848fbd6de4a
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-03-17 16:10:13 +0000

    SDIT-3597: 🔒️ Pin swagger-ui for dompurify issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "10.0.5"
+version = "10.0.6"
 
 gradlePlugin {
   website.set("https://github.com/ministryofjustice/hmpps-gradle-spring-boot")
