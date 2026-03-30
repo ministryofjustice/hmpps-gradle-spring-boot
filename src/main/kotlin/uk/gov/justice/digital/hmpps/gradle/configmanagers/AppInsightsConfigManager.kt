0888bba3194277dc0dd7d7dd5bb0e106ab05a2b6
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-03-30 13:53:35 +0100

    SDIT-3635: ⬆️ Upgrade app insights to 3.7.8

--- src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/configmanagers/AppInsightsConfigManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/configmanagers/AppInsightsConfigManager.kt
@@ -4,11 +4,11 @@ import org.gradle.api.Project
 import org.gradle.api.tasks.Copy
 import uk.gov.justice.digital.hmpps.gradle.ConfigManager
 
-private const val APP_INSIGHTS_VERSION = "3.7.7"
+private const val APP_INSIGHTS_VERSION = "3.7.8"
 
 // This should be kept at the same version as the SDK version used by App Insights in whatever version is specified
-// above with APP_INSIGHTS_VERSION, e.g. https://github.com/microsoft/ApplicationInsights-Java/blob/3.7.7/dependencyManagement/build.gradle.kts#L10
-const val OPENTELEMETRY_VERSION = "1.55.0"
+// above with APP_INSIGHTS_VERSION, e.g. https://github.com/microsoft/ApplicationInsights-Java/blob/3.7.8/dependencyManagement/build.gradle.kts#L10
+const val OPENTELEMETRY_VERSION = "1.60.1"
 
 class AppInsightsConfigManager(override val project: Project) : ConfigManager {
   override fun configure() {
