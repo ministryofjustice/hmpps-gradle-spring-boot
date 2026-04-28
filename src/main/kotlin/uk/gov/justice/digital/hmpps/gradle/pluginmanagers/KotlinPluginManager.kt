058ccbb4c96150997c46dc429119e100d62da01f
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-04-28 08:52:04 +0100

    SDIT-3744: ⬆️ Upgrade to latest spring boot 4.0.6 etc. (#420)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/KotlinPluginManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/KotlinPluginManager.kt
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import uk.gov.justice.digital.hmpps.gradle.PluginManager
 
-const val JACKSON_VERSION = "3.1.0"
+const val JACKSON_VERSION = "3.1.2"
 const val JACKSON2_VERSION = "2.21.2"
 
 class KotlinPluginManager(override val project: Project) : PluginManager {
@@ -28,6 +28,6 @@ class KotlinPluginManager(override val project: Project) : PluginManager {
     project.dependencies.add("implementation", "com.fasterxml.jackson.module:jackson-module-kotlin:$JACKSON2_VERSION")
     project.dependencies.add("implementation", "org.jetbrains.kotlin:kotlin-reflect")
 
-    project.dependencies.add("testImplementation", "org.mockito.kotlin:mockito-kotlin:6.2.3")
+    project.dependencies.add("testImplementation", "org.mockito.kotlin:mockito-kotlin:6.3.0")
   }
 }
