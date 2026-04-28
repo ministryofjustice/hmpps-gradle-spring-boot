6418e1863646f9d959a393f969dd3b4939fef8e0
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-04-28 11:56:50 +0100

    SDIT-3744: 📌 Unpin tomcat and let float version (#421)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
@@ -14,7 +14,6 @@ class DependencyManagementPluginManager(override val project: Project) : PluginM
   override fun configure() {
     applyDependencyManagementBom(project)
     project.extensions.extraProperties["opentelemetry.version"] = OPENTELEMETRY_VERSION
-    project.extensions.extraProperties["tomcat.version"] = "11.0.21"
   }
 
   private fun applyDependencyManagementBom(project: Project) {
