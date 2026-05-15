e88010b28f3abaa5b25a141c48f3e3e5ab7ccead
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-05-15 08:44:35 +0100

    SDIT-3807: 🔒️ Upgrade tomcat for security issues (#426)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
@@ -15,6 +15,7 @@ class DependencyManagementPluginManager(override val project: Project) : PluginM
     applyDependencyManagementBom(project)
     project.extensions.extraProperties["opentelemetry.version"] = OPENTELEMETRY_VERSION
     project.extensions.extraProperties["netty.version"] = "4.2.13.Final"
+    project.extensions.extraProperties["tomcat.version"] = "11.0.22"
   }
 
   private fun applyDependencyManagementBom(project: Project) {
