462a28e180c1d2b92d65a3b131cb6e46e0408f19
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-08-21 11:21:23 +0100

    Revert OWASP dep update as this will require a separate PR, also add suppression for dompurify issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
   id("maven-publish")
   id("com.github.ben-manes.versions") version "0.61.0"
   id("se.patrikerdes.use-latest-versions") version "0.2.19"
-  id("org.owasp.dependencycheck") version "13.0.0"
+  id("org.owasp.dependencycheck") version "12.2.2"
   id("com.adarshr.test-logger") version "4.0.0"
   id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
 }
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "11.1.0"
+version = "11.0.6"
 
 gradlePlugin {
   website.set("https://github.com/ministryofjustice/hmpps-gradle-spring-boot")
@@ -55,8 +55,8 @@ dependencies {
 
   implementation("org.springframework.boot:spring-boot-gradle-plugin:4.1.1")
   implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.1.7")
-  implementation("org.owasp:dependency-check-core:13.0.0")
-  implementation("org.owasp:dependency-check-gradle:13.0.0")
+  implementation("org.owasp:dependency-check-core:12.2.2")
+  implementation("org.owasp:dependency-check-gradle:12.2.2")
   implementation("com.github.ben-manes:gradle-versions-plugin:0.54.0")
   implementation("com.gorylenko.gradle-git-properties:com.gorylenko.gradle-git-properties.gradle.plugin:4.0.1")
   implementation("com.adarshr.test-logger:com.adarshr.test-logger.gradle.plugin:4.0.0")
