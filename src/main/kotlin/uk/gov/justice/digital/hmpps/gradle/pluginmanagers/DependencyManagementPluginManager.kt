bb1dfc76c9145cb02fa9450a13337bda1a6a7189
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-09-01 13:29:47 +0100

    SDIT-4185: 🔒️ Pin tomcat and suppress opentelemetry (#446)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
@@ -14,6 +14,8 @@ class DependencyManagementPluginManager(override val project: Project) : PluginM
   override fun configure() {
     applyDependencyManagementBom(project)
     project.extensions.extraProperties["opentelemetry.version"] = OPENTELEMETRY_VERSION
+
+    project.extensions.extraProperties["tomcat.version"] = "11.0.25"
   }
 
   private fun applyDependencyManagementBom(project: Project) {
